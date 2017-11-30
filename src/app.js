e6065ab29aa24e7082f6f3e6b735864f72504fba
Sergey Okhrymenko <mr.inceon@gmail.com>
2017-11-30 23:26:36 +0200

    added click event and functionality for drawing circle

--- src/app.js
+++ src/app.js
@@ -1,20 +1,39 @@
 //Create the renderer
-Engine.game = PIXI.autoDetectRenderer(Engine.maxWidth, Engine.maxHeight);
+Engine.game = new PIXI.Application(
+    Engine.maxWidth, Engine.maxHeight, Engine.config
+);
+
+Engine.game.backgroundColor = 0x000032;
 
 //Add the canvas to the HTML document
 document.body.appendChild(Engine.game.view);
 
-var stage = new PIXI.Container();
-var circle = new PIXI.Graphics();
-circle.lineStyle(0);
-circle.beginFill(0xf2f233, 1);
-circle.drawCircle(550, 550, 50);
-circle.endFill();
-circle.interactive = true;
-circle.buttonMode = true;
-stage.addChild(circle);
-circle.on('pointerdown', () => {
-    console.log(5555555);
-});
+var rect = new PIXI.Graphics();
+    rect.beginFill(0x000011, 1);
+    rect.drawRect(0, 0, Engine.maxWidth, Engine.maxHeight);
 
-Engine.game.render(stage);
+Engine.game.stage.interactive = true;
+Engine.game.stage.click = (ev) => {
+    newRect(ev.data.global);
+};
+
+Engine.game.stage.addChild(rect);
+
+var figures = [];
+var gravity = 2;
+function newRect(data) {
+    let rect = new PIXI.Graphics();
+        rect.lineStyle(0);
+        rect.beginFill(0x990099, 0.5);
+        rect.drawCircle(data.x, data.y, 60);
+        rect.endFill();
+
+    Engine.game.stage.addChild(rect);
+
+    Engine.game.ticker.add(() => {
+        rect.position.y += gravity;
+        console.log('tick');
+    });
+}
+    
+// Engine.game.render(stage);
--- src/engine.js
+++ src/engine.js
@@ -1,6 +1,11 @@
 var Engine = {
-  minWidth: 640,
-  minHeight: 320,
-  maxWidth: window.innerWidth,
-  maxHeight: window.innerHeight,
+    minWidth: 640,
+    minHeight: 320,
+    maxWidth: window.innerWidth - 50,
+    maxHeight: window.innerHeight - 50,
+    config: {
+        antialias: false,
+        transparent: false,
+        resolution: 1
+    }
 };
