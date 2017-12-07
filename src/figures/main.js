deda24dd093b22bf38840cb262b7dd6325a99c48
Sergey Okhrymenko <mr.inceon@gmail.com>
2017-12-07 10:49:16 +0200

    added comment in Figure class

--- src/figures/main.js
+++ src/figures/main.js
@@ -1,14 +1,24 @@
+/**
+ * Main class for figures
+ * Contain three main functions
+ */
 class Figure {
     constructor(){
         this.ticker = new PIXI.ticker.Ticker();
         this.ticker.add(this.applyGravity.bind(this));
     }
 
+    /**
+     * Function checks whether the ticker is running
+     * and and applies gravity for figures
+     */
     applyGravity() {
         if (this.ticker.started) {
             this.model.position.y += Engine.gravity;
+
+            // If the figure outside the game area removes it
             if (this.model.position.y > Engine.game.screen.height) {
-                this.ticker.destroy();
+                this.click();
             }
         }
     }
